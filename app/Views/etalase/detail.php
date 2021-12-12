a3f42778078f860093d198fd9b037df175e51997
teguhedip <teguhediprasetyo@gmail.com>
2021-12-12 22:36:01 +0700

    berhasil menambahkan untuk user publick dan card

--- app/Views/etalase/detail.php
+++ app/Views/etalase/detail.php
@@ -2,29 +2,23 @@
 
 <?= $this->section('content') ?>
 
-<table class="table">
-    <thead>
-        <tr>
-            <th scope="col">Brand >> </th>
-            <th scope="col"><?= $lp['brand'] ?></th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <th scope="row">Tipe >> </th>
-            <td><?= $lp['type'] ?></td>
-        </tr>
-        <tr>
-            <th scope="row">Status >> </th>
-            <td><?= $lp['status'] ?></td>
-        </tr>
-        <tr>
-            <th>
-                <a href="<?= base_url('/') ?>" class="btn btn-primary btn-sm" target="">Kembali</a>
-            </th>
-            <th></th>
-        </tr>
-    </tbody>
-</table>
+<div class="card" style="width: 18rem;">
+    <img src="<?= base_url('img') ?>/laptop.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">Detail Laptop <?= $lp['brand'] . " " .  $lp['type']  ?></h5>
+        <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+    </div>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">Type : <?= $lp['type']  ?></li>
+        <li class="list-group-item">Status : <?= $lp['status']  ?></li>
+        <li class="list-group-item">Sistem Operasi : <?= $lp['sistem_operasi']  ?></li>
+        <li class="list-group-item">Prosesor : <?= $lp['processor']  ?></li>
+        <li class="list-group-item">Display : <?= $lp['display']  ?></li>
+    </ul>
+    <div class="card-body">
+        <a href="<?= base_url('/') ?>" class="card-link">Kembali</a>
+        <!-- <a href="#" class="card-link">Another link</a> -->
+    </div>
+</div>
 
 <?= $this->endSection() ?>
